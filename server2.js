a8b7188bcfa92436e88a71080d5e3bc58c8f0a3a
lokeshkumar9600 <lokesh960077152@gmail.com>
2022-09-07 18:21:56 +0530

    added

---
+++ server2.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+
+app.get("/getDetails/:empid",(req, res) =>{
+    
+});
+
+app.listen("3001",(req,res) => {
+   console.log("details server is running");
+});
